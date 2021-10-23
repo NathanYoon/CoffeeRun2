aefce09c36cbe5174a8d9835876e5d3dc14d587a
Elnathan Yoon <60671231+NathanYoon@users.noreply.github.com>
2021-10-22 22:05:29 -0700

    Add files via upload

---
+++ scripts/firebaseconfig.js
@@ -0,0 +1,24 @@
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+(function (window) {
+    'use strict';
+  //   var App = window.App || {};
+    
+      window.FirebaseConfig = {
+        apiKey: "a string from Firestore",
+        authDomain: "a string from Firestore",
+        projectId: "a string from Firestore",
+        storageBucket: "a string from Firestore",
+        messagingSenderId: "a string from Firestore",
+        appId: "a string from Firestore",
+        measurementId: "a string from Firestore"
+      };
+  //   App.FirebaseConfig = FirebaseConfig;
+  //   window.App = App;
+  
+  })(window);
