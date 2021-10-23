9156c5e66c633b73ba34f69d01a59d26db633176
Elnathan Yoon <60671231+NathanYoon@users.noreply.github.com>
2021-10-22 23:52:37 -0700

    Add files via upload

--- scripts/firebaseconfig.js
+++ scripts/firebaseconfig.js
@@ -7,18 +7,21 @@
 
 (function (window) {
     'use strict';
-  //   var App = window.App || {};
+  var App = window.App || {};
     
-      window.FirebaseConfig = {
-        apiKey: "AIzaSyBDUuPj-j1uOgee_1WDSlzRdZHDMSo1VsQ",
-        authDomain: "coffeerun-7dfab.firebaseapp.com",
-        projectId: "coffeerun-7dfab",
-        storageBucket: "coffeerun-7dfab.appspot.com",
-        messagingSenderId: "642832046936",
-        appId: "1:642832046936:web:2a878c1cc54aabbb0c232d",
-        measurementId: "G-RG76BC5RBZ"
-      };
-  //   App.FirebaseConfig = FirebaseConfig;
-  //   window.App = App;
+  var FirebaseConfig = {
+    apiKey: "AIzaSyBDUuPj-j1uOgee_1WDSlzRdZHDMSo1VsQ",
+    authDomain: "coffeerun-7dfab.firebaseapp.com",
+    projectId: "coffeerun-7dfab",
+    storageBucket: "coffeerun-7dfab.appspot.com",
+    messagingSenderId: "642832046936",
+    appId: "1:642832046936:web:2a878c1cc54aabbb0c232d",
+    measurementId: "G-RG76BC5RBZ"
+  };
+
+  App.FirebaseConfig = FirebaseConfig;
+  FirebaseConfig.initializeApp(App.FirebaseConfig);
+
+  window.App = App;
   
   })(window);
